725106a92432865df4ff0191856e8487e3e72e9a
suragaryen <jennet967@gmail.com>
2023-12-13 16:13:13 +0900

    first commit

---
+++ tindog_project/src/main/java/kr/co/tindog/TindogApplication.java
@@ -0,0 +1,13 @@
+package kr.co.tindog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TindogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TindogApplication.class, args);
+	}
+
+}
