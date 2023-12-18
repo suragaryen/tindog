0c75ae4c18af437c0e0492ef9185d7ec419d5c6a
suragaryen <jennet967@gmail.com>
2023-12-18 12:21:40 +0900

    login-register ver1

--- tindog_project/src/main/java/kr/co/tindog/controller/BoardController.java
+++ tindog_project/src/main/java/kr/co/tindog/controller/BoardController.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.ModelAndView;
 @RestController
 public class BoardController {
 
-	@RequestMapping("/")
+	@RequestMapping("/") //@Controller와 @ResponseBode를 합친 것과 같은 역할을 하며 JSON, XML 또는 일반텍스트와 같은 다양한 유형의 응답을 생
 	public ModelAndView home() {
 		ModelAndView mav=new ModelAndView();
 		mav.setViewName("pages/home");
@@ -15,4 +15,19 @@ public class BoardController {
 		
 	}
 	
-}
+	@RequestMapping("/login")
+	public ModelAndView login() {
+		ModelAndView mav=new ModelAndView();
+		mav.setViewName("pages/login");
+		return mav;
+		
+	}
+	
+	@RequestMapping("/register")
+	public ModelAndView register() {
+		ModelAndView mav=new ModelAndView();
+		mav.setViewName("pages/register");
+		return mav;
+		
+	}
+}//class
