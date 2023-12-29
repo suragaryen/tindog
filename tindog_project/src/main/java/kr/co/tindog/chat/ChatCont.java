8535f159fd3357801c5c33d34d4ae9580f5e29da
zunmoon <vosejtjdans@gmail.com>
2023-12-29 14:43:00 +0900

    first commit

--- tindog_project/src/main/java/kr/co/tindog/chat/ChatCont.java
+++ tindog_project/src/main/java/kr/co/tindog/chat/ChatCont.java
@@ -7,6 +7,11 @@ import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import jakarta.servlet.http.HttpSession;
@@ -31,9 +36,26 @@ public class ChatCont {
     }
     
     @GetMapping("/chat/{droomno}")
-	public ModelAndView chat(HttpSession session) {
+	public ModelAndView chat(HttpSession session, @PathVariable int droomno) {
 		ModelAndView mav = new ModelAndView();
+		session.setAttribute("s_droomno", droomno);
 		mav.setViewName("layout/mypage/chat");
+		mav.addObject("list", chatDao.chatLogs(droomno));
+		System.out.println(chatDao.chatLogs(droomno));
 		return mav;
 	}
+    
+    @PostMapping("/chat/chatLogInsert")
+    @ResponseBody
+    public int chatLogsInsert(@RequestParam int droomno, @RequestParam String message, HttpSession session) {
+    	ChatDTO chatDto = new ChatDTO();
+    	chatDto.setDroomno(droomno);
+    	chatDto.setMessage(message);
+    	chatDto.setSender((String)session.getAttribute("s_nickname"));
+    	
+    	int cnt = chatDao.chatInsert(chatDto);
+    	
+    	return cnt;
+    }
+    
 }
--- tindog_project/src/main/java/kr/co/tindog/chat/ChatDAO.java
+++ tindog_project/src/main/java/kr/co/tindog/chat/ChatDAO.java
@@ -1,5 +1,8 @@
 package kr.co.tindog.chat;
 
+import java.util.List;
+import java.util.Map;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -8,5 +11,13 @@ import org.springframework.stereotype.Repository;
 public class ChatDAO {
 	
 	@Autowired
-	SqlSession selSession;
+	SqlSession sqlSession;
+	
+	public List<Map<String, Object>> chatLogs(int droomno) {
+		return sqlSession.selectList("chat.chatLogs", droomno);
+	}
+	
+	public int chatInsert(ChatDTO chatDto) {
+		return sqlSession.insert("chat.chatLogInsert", chatDto);
+	}
 }
